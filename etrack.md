426d45bc2eea8f191c9b8d403fc00f9c7f668e25
Unlqsting <noorbijapur500@gmail.com>
2023-01-16 11:49:19 -0800

    frontend prog

---
+++ etrack.md
@@ -0,0 +1,15 @@
+<body>
+<div class="workoutWrap">
+<header class="header">
+    <h1>Track your workout</h1>
+    <button class="addWorkout">Add Workout</button>
+</header>
+
+<h2>Workouts</h2>
+
+<script src="track.js"></script>
+
+<link rel="stylesheet" href="track.css">
+</div>
+    
+</body>
---
+++ etrack/track.js
@@ -0,0 +1,31 @@
+const addWorkout = document.querySelector(".addWorkout")
+
+const divCont = document.querySelector(".workoutWrap")
+
+const chestButton = document.getElementById("chest")
+
+const exerButton = document.querySelector("dropdown-content")
+
+// exerButton.addEventListener("click", chestbuttonSwitch())
+
+function chestbuttonSwitch(){
+    exerButton.innerText = chestButton.innerText
+}
+
+workoutPreview = '<div class="dropdown">' +
+'<button class="dropbtn">Choose Exercise</button>' +
+'<div class="dropdown-content">' +
+'<button onclick="chestbuttonSwitch()" id="chest">Chest</button>' +
+'<a class="back">Back</a>' +
+'<a class="legs">Legs</a>' +
+'</div>' +
+'</div>' 
+
+function addNew(){
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("workouts")
+    divCont.appendChild(newDiv);
+    newDiv.innerHTML = workoutPreview
+}
+
+addWorkout.addEventListener("click", addNew);
