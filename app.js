252be4f9423c055e2ded2144917e3064cfb32eed
Unlqsting <noorbijapur500@gmail.com>
2023-02-03 13:18:54 -0800

    tracker

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-logIn = document.querySelector('.logInform');
-name = document.querySelector('.name');
-uid = document.querySelector('.uid');
-password = document.querySelector('.password');
+// logIn = document.querySelector('.logInform');
+// name = document.querySelector('.name');
+// uid = document.querySelector('.uid');
+// password = document.querySelector('.password');
 
-logIn.addEventListener('submit', (e) => {
-    submitForm(e, logIn);
-});
+// logIn.addEventListener('submit', (e) => {
+//     submitForm(e, logIn);
+// });
 
-buildjsonForm(form) {
-    formData = {
-        "name": name.value,
-        "uid": uid.value,
-        "password": password.value
-    };
-    return formData;
-};
+// buildjsonForm(form) {
+//     formData = {
+//         "name": name.value,
+//         "uid": uid.value,
+//         "password": password.value
+//     };
+//     return formData;
+// };
 
-async function submitForm(e, form) {
-    e.preventDefault();
+// async function submitForm(e, form) {
+//     e.preventDefault();
     
-    const submitBtn = form.querySelector('button[type="submit"]');
-    const jsonForm = buildjsonForm(form);
-    const header = newHeaders();
-    const res = await fetchService.performPostHttpRequest('http://127.0.0.1:8086/api/users/create', jsonForm, header);
+//     const submitBtn = form.querySelector('button[type="submit"]');
+//     const jsonForm = buildjsonForm(form);
+//     const header = newHeaders();
+//     const res = await fetchService.performPostHttpRequest('http://127.0.0.1:8086/api/users/create', jsonForm, header);
 
-}   
+// }   
