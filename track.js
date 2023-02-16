16a9ad1116ebcc769454a4b34a734ae34b13aa0c
Nathan Capule <nathancapule13@gmail.com>
2023-02-16 09:55:10 -0800

    i broke it

--- track.js
+++ track.js
@@ -1,110 +1,131 @@
 const addWorkout = document.querySelector(".addWorkout");
 const divCont = document.querySelector(".workoutWrap");
 
-const exerciseOptions = '<div class="exercise-options" style="display: none;">' +
-    '<button class="exercise-option chest-btn">Chest</button>' +
-    '<button class="exercise-option back-btn">Back</button>' +
-    '<button class="exercise-option legs-btn">Legs</button>' +
-    '<button class="exercise-option other-btn">Other</button>' +
-    '</div>';
+workoutPreview = '<div class="workout">' +
+  '<button class="choose-exercise choose-exercise-btn">Choose Exercise</button>' +
+  '<div class="exercise-options" style="display: none;">' +
+  '<button class="exercise-option chest-btn">Chest</button>' +
+  '<button class="exercise-option back-btn">Back</button>' +
+  '<button class="exercise-option legs-btn">Legs</button>' +
+  '<button class="exercise-option other-btn">Other</button>' +
+  '</div>' +
+  '<button class="choose-reps choose-reps-btn">Reps</button>' +
+  '<div class="rep-options" style="display: none;">' +
+  '<button class="rep-option five-btn">5 reps</button>' +
+  '<button class="rep-option ten-btn">10 reps</button>' +
+  '<button class="rep-option fifteen-btn">15 reps</button>' +
+  '</div>' +
+  '<button class="choose-sets choose-sets-btn">Sets</button>' +
+  '<div class="set-options" style="display: none;">' +
+  '<button class="set-option fives-btn">5 Sets</button>' +
+  '<button class="set-option tens-btn">10 Sets</button>' +
+  '<button class="set-option fifteens-btn">15 Sets</button>' +
+  '</div>' +
+'</div>' +
+'<button class="removeWorkout">Remove</button>';
 
-const repOptions = '<div class="rep-options" style="display: none;">' +
-    '<button class="rep-option five-btn">5 reps</button>' +
-    '<button class="rep-option ten-btn">10 reps</button>' +
-    '<button class="rep-option fifteen-btn">15 reps</button>' +
-    '</div>';
-
-const setOptions = '<div class="set-options" style="display: none;">' +
-    '<button class="set-option fives-btn">5 Sets</button>' +
-    '<button class="set-option tens-btn">10 Sets</button>' +
-    '<button class="set-option fifteens-btn">15 Sets</button>' +
-    '</div>';
-
-const workoutPreview = `<div class="workout">
-    <button class="choose-exercise choose-exercise-btn">Choose Exercise</button>
-    ${exerciseOptions}
-    <button class="choose-reps choose-reps-btn">Reps</button>
-    ${repOptions}
-    <button class="choose-sets choose-sets-btn">Sets</button>
-    ${setOptions}
-    <button class="removeWorkout">Remove</button>
-</div>`;
-
-function addNew() {
+function addNew(){
     const newDiv = document.createElement("div");
     newDiv.classList.add("workouts");
     divCont.appendChild(newDiv);
     newDiv.innerHTML = workoutPreview;
-
-    newDiv.querySelector(".removeWorkout").addEventListener("click", function() {
+    
+    const removeButton = newDiv.querySelector(".removeWorkout");
+    removeButton.addEventListener("click", function() {
         newDiv.remove();
     });
 
-    newDiv.querySelector(".choose-exercise").addEventListener("click", function() {
-        newDiv.querySelector(".exercise-options").style.display = toggleDisplay(newDiv.querySelector(".exercise-options").style.display);
+    const chooseExercise = newDiv.querySelector(".choose-exercise");
+    chooseExercise.addEventListener("click", function() {
+        const exerciseOptions = newDiv.querySelector(".exercise-options");
+        if (exerciseOptions.style.display === "none") {
+            exerciseOptions.style.display = "block";
+        } else {
+            exerciseOptions.style.display = "none";
+        }
     });
 
-    newDiv.querySelector(".choose-reps").addEventListener("click", function() {
-        newDiv.querySelector(".rep-options").style.display = toggleDisplay(newDiv.querySelector(".rep-options").style.display);
-    });
-    
-    newDiv.querySelector(".choose-sets").addEventListener("click", function() {
-        newDiv.querySelector(".set-options").style.display = toggleDisplay(newDiv.querySelector(".set-options").style.display);
-    });
-
-    newDiv.querySelectorAll(".exercise-option").forEach(function(option) {
-        option.addEventListener("click", function() {
-            newDiv.querySelector(".choose-exercise").innerText = option.innerText;
-            newDiv.querySelector(".exercise-options").style.display = "none";
-
-            // Send HTTP POST request to update the workout
-            const data = { workout: option.innerText };
-            fetch('http://127.0.0.1:8086/api/users/', {
-              method: 'PATCH',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(data)
-            })
-            .then(response => console.log('Workout updated successfully'))
-            .catch(error => console.error('Error updating workout', error));
-        });
-    });
-    
-    newDiv.querySelectorAll(".rep-option").forEach(function(option) {
-        option.addEventListener("click", function() {
-            newDiv.querySelector(".choose-rep").innerText = option.innerText;
-            newDiv.querySelector(".rep-options").style.display = "none";
-
-            // Send HTTP POST request to update the workout
-            const data = { workout: option.innerText };
-            fetch('http://127.0.0.1:8086/api/users/', {
-              method: 'PATCH',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(data)
-            })
-            .then(response => console.log('Workout updated successfully'))
-            .catch(error => console.error('Error updating workout', error));
-        });
+    const chooseReps = newDiv.querySelector(".choose-reps");
+    chooseReps.addEventListener("click", function() {
+        const repOptions = newDiv.querySelector(".rep-options");
+        if (repOptions.style.display === "none") {
+            repOptions.style.display = "block";
+        } else {
+            repOptions.style.display = "none";
+        }
     });
 
-    newDiv.querySelectorAll(".set-option").forEach(function(option) {
-        option.addEventListener("click", function() {
-            newDiv.querySelector(".choose-set").innerText = option.innerText;
-            newDiv.querySelector(".set-options").style.display = "none";
+    const chooseSets = newDiv.querySelector(".choose-sets");
+    chooseSets.addEventListener("click", function() {
+        const setOptions = newDiv.querySelector(".set-options");
+        if (setOptions.style.display === "none") {
+            setOptions.style.display = "block";
+        } else {
+            setOptions.style.display = "none";
+        }
+    });
 
-            // Send HTTP POST request to update the workout
-            const data = { workout: option.innerText };
-            fetch('http://127.0.0.1:8086/api/users/', {
-              method: 'PATCH',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(data)
-            })
-            .then(response => console.log('Workout updated successfully'))
-            .catch(error => console.error('Error updating workout', error));
-        });
-    })}
+    const chestButton = newDiv.querySelector(".chest-btn");
+    chestButton.addEventListener("click", function() {
+        chooseExercise.innerText = chestButton.innerText;
+        exerciseOptions.style.display = "none";
+    });
+
+    const backButton = newDiv.querySelector(".back-btn");
+    backButton.addEventListener("click", function() {
+        chooseExercise.innerText = backButton.innerText;
+        exerciseOptions.style.display = "none";
+    });
+
+    const legsButton = newDiv.querySelector(".legs-btn");
+    legsButton.addEventListener("click", function() {
+        chooseExercise.innerText = legsButton.innerText;
+        exerciseOptions.style.display = "none";
+    });
+
+    const otherButton = newDiv.querySelector(".other-btn");
+    otherButton.addEventListener("click", function() {
+        chooseExercise.innerText = otherButton.innerText;
+        exerciseOptions.style.display = "none";
+    });
+  
+    const fiveButton = newDiv.querySelector(".five-btn");
+    fiveButton.addEventListener("click", function() {
+        chooseReps.innerText = fiveButton.innerText;
+        repOptions.style.display = "none";
+    });
+
+    const tenButton = newDiv.querySelector(".ten-btn");
+    tenButton.addEventListener("click", function() {
+        chooseReps.innerText = tenButton.innerText;
+        repOptions.style.display = "none";
+    });
+
+    const fifteenButton = newDiv.querySelector(".fifteen-btn");
+    fifteenButton.addEventListener("click", function() {
+        chooseReps.innerText = fifteenButton.innerText;
+        repOptions.style.display = "none";
+
+    });
+
+    const fivesButton = newDiv.querySelector(".fives-btn");
+    fivesButton.addEventListener("click", function() {
+        chooseSets.innerText = fivesButton.innerText;
+        setOptions.style.display = "none";
+    });
+
+    const tensButton = newDiv.querySelector(".tens-btn");
+    tensButton.addEventListener("click", function() {
+        chooseSets.innerText = tensButton.innerText;
+        setOptions.style.display = "none";
+    });
+
+    const fifteensButton = newDiv.querySelector(".fifteens-btn");
+    fifteensButton.addEventListener("click", function() {
+        chooseSets.innerText = fifteensButton.innerText;
+        setOptions.style.display = "none";
+    });
+
+}
+
+addWorkout.addEventListener("click", addNew);
