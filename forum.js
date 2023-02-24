560fe67f80ef3feef5209eafbc19a3def98cba29
Unlqsting <noorbijapur500@gmail.com>
2023-02-23 18:56:00 -0800

    update worked ;)

--- forum.js
+++ forum.js
@@ -26,27 +26,76 @@ function createPost(data) {
 
         // Add click event listener to display modal window when post is clicked
         newDiv.addEventListener('click', function() {
-        document.getElementById('postModal').style.display = "block";
-        document.getElementById('modalPostTitle').innerHTML = "Title: " + post.postTitle;
-        document.getElementById('modalPostText').innerHTML = "Post: " + post.post;
+          // ! set modal elements to variables
+          const modalPostTitle = document.getElementById('modalPostTitle')
+          const modalPostText = document.getElementById('modalPostText')
+          
+
+          //? display modal window as block element
+          document.getElementById('postModal').style.display = "block";   
+          
+          //! set modal elements to post data
+          modalPostTitle.innerHTML = "Post Title: " + '<input type="text" name="updatedPostTitle" id="updatedPostTitle" value="' + post.postTitle + '">';
+          modalPostText.innerHTML = "Post: " + '<input type="text" name="updatedPost" id="updatedPost" value="' + post.post + '">';
+          
+          updatedPostTitle = modalPostTitle.innerText;
+          updatedPost = modalPostText.innerText;
+
+          id = post.id; // Set id to the id of the clicked post
+
         });
     }
-  }
+}
 
-  //! Display User Table, data is fetched from Backend Database
+
+
+//! Display User Table, data is fetched from Backend Database
 function read_posts() {
     $.getJSON('http://127.0.0.1:8086/api/forum/', function(data) {
         createPost(JSON.stringify(data));
+        id = data.id;
     });
 }
 
+// ! update post
+const updateButton = document.getElementById('updateButton');
+
+updateButton.addEventListener('click', function() {
+  // Get the updated post title and content from the input fields
+  const updatedPostTitle = document.getElementById('updatedPostTitle').value;
+  const updatedPost = document.getElementById('updatedPost').value;
+
+  // Send a PUT request to the backend API to update the post with the new title and content
+  fetch('http://127.0.0.1:8086/api/forum/update', {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ postTitle: updatedPostTitle, post: updatedPost, id: id}),
+  })
+  .then(response => {
+    if (response.ok) {
+      // Reload the page to display the updated post
+      window.location.reload();
+    }
+  });
+
+});
+
+
 // ! modal stuff
 const modal = document.getElementById("postModal");
-const closeBtn = document.getElementsByClassName("close")[0];
+const closeBtn = document.querySelector('#closeBtn')
+
+
+
 
 // Close the modal window when the user clicks the close button or clicks outside the modal window
 closeBtn.onclick = function() {
   modal.style.display = "none";
+
+  //! reload page to reset modalTitle and modalPosttext to original values
+  window.location.reload();
 };
 
 window.onclick = function(event) {
@@ -54,4 +103,3 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 };
-    
--- forum.md
+++ forum.md
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forum</title>
     <link rel="stylesheet" href="forum.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body>
 <!-- action="http://127.0.0.1:8086/api/forum/create" target="_blank" -->
@@ -19,16 +20,20 @@
             <input type="text" name="topic" id="topic" class="topic" size=40 maxlength=50 placeholder="Topic title">
             <P><strong>Post Text:</strong><br>
             <input type="text" id="postText" name="postText" class="postText" >
+            <br>
             <button type="submit" class="topicSubmit" id="submitForm">Add Topic</button>
         <!--  -->
     <!-- </div> -->
     <div id="postModal" class="modal">
         <div class="modal-content">
-            <span class="close">&times;</span>
+            <span id="closebtn" class="close">&times;</span>
             <h3 id="modalPostTitle"></h3>
             <p id="modalPostText"></p>
+            <button type="button" class="updateButton" id="updateButton">Update</button>
         </div>
     </div>
 <!-- </body> -->
 <script src="forum.js">
+
+
         
