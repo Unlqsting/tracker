a3f27195f6c83ff1750f8eff63beebbe811208eb
SAnd353 <91165049+SAnd353@users.noreply.github.com>
2023-02-25 15:51:15 -0800

    Rewriting code to meet CRUD requirements

--- calendar.js
+++ calendar.js
@@ -2,13 +2,10 @@ var savedEvents = {
     
 }
 
-function PATCH_notemtpyvalues() {
-    fetch("https://lennsflask.duckdns.org/api/etrack_users/update", {
-        method: "PATCH",
-        body: JSON.stringify({
-            uname: "testUser",
-            savedWorkouts: notemptyvalues,
-        }),
+function send_Data(endpoint, method, body) {
+    fetch("http://localhost:8086/api/etrack_users/" + endpoint, {
+        method: method,
+        body: JSON.stringify(body),
         headers: {
             "Content-type": "application/json; charset=UTF-8"
         }
@@ -23,12 +20,21 @@ function updateCal() {
     document.getElementById("scheduledevents").innerHTML = '';
     if (document.getElementById('eventTitleInput').value.length > 0) {
         console.log("updating events");
-        savedEvents[clicked + ' ' + document.getElementById("calendarHeader").innerText].push(document.getElementById('eventTitleInput').value);
-        
+        dateK = clicked + monthAndYear;
+        workoutsV = document.getElementById('eventTitleInput').value;
+        savedEvents[dateK].push(workoutsV);
+        if (dateK in notemptyvalues) {
+            send_Data("update", "PATCH", {date: dateK, savedWorkouts: workoutsV});
+            console.log("patching");
+        }
+        else {
+            send_Data("create", "POST", {date: dateK, savedWorkouts: workoutsV});
+            console.log("posting");
+        }
     }
     document.getElementById('eventTitleInput').value = ''
     applySavedData();
-    PATCH_request();
+    // send_Data();
 }
 
 const newEventPopup = document.getElementById('newEventPopup');
@@ -51,92 +57,94 @@ function openPopup(date) {
 }
 
 function closePopup() {
-editEventPopup.style.display = 'none';
-newEventPopup.style.display= 'none';
-backDrop.style.display = 'none';
+    editEventPopup.style.display = 'none';
+    newEventPopup.style.display= 'none';
+    backDrop.style.display = 'none';
 }
 
 let monthNav = 0;
 let clicked = null;
 // let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
+
 const calendar = document.getElementById('calendar');
 
 function renderCalendar() {
 
-const date = new Date();
+    const date = new Date();
 
-const day = date.getDate();
-const month = date.getMonth();
-const year = date.getFullYear();
-const daysInMonth = new Date(year, month + 1, 0).getDate();
-const firstDayOfMonth = new Date(year, month, 1);
-const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const firstDayOfMonth = new Date(year, month, 1);
+    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-const dateString = firstDayOfMonth.toLocaleDateString('en-US', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric', 
+    const dateString = firstDayOfMonth.toLocaleDateString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric', 
 
-});
-
-
-
-document.getElementById('calendarHeader').innerText = 
-`${date.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+    });
 
 
-calendar.innerHTML = '';
 
-const prevDays = weekdays.indexOf(dateString.split(', ')[0]);
-for(let i = 1; i <= prevDays + daysInMonth; i++) {
-    const daySquare = document.createElement('div');
-    daySquare.classList.add('day');
+    document.getElementById('calendarHeader').innerText = 
+    `${date.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
 
-    if (i > prevDays) {
-        daySquare.innerText = i - prevDays;
-        daySquare.setAttribute("id", i - prevDays)
-        daySquare.addEventListener('click', () => openPopup(daySquare.innerHTML));
-        savedEvents[daySquare.innerHTML + ' ' + document.getElementById("calendarHeader").innerText] = Array();
-    } 
-    else {
-        daySquare.classList.add('padding');
+
+    calendar.innerHTML = '';
+
+    const prevDays = weekdays.indexOf(dateString.split(', ')[0]);
+    for(let i = 1; i <= prevDays + daysInMonth; i++) {
+        const daySquare = document.createElement('div');
+        daySquare.classList.add('day');
+
+        if (i > prevDays) {
+            daySquare.innerText = i - prevDays;
+            daySquare.setAttribute("id", i - prevDays)
+            daySquare.addEventListener('click', () => openPopup(daySquare.innerHTML));
+            savedEvents[daySquare.innerHTML + ' ' + document.getElementById("calendarHeader").innerText] = Array();
+        } 
+        else {
+            daySquare.classList.add('padding');
+        }
+    calendar.appendChild(daySquare);
     }
-calendar.appendChild(daySquare);
-}
 
 } 
 
 function clearWorkouts() {
     console.log(savedEvents);
+    targetDates = [];
+    console.log(notemptyvalues);
+    for (const k in notemptyvalues) {
+        console.log("Pushing " + k)
+        targetDates.push(k);
+    }
     for(var day in savedEvents) {
         savedEvents[day] = [];
         console.log("cleared " + savedEvents)
     }
     applySavedData();
-    fetch("https://lennsflask.duckdns.org/api/etrack_users/update", {
-        method: "PATCH",
-        body: JSON.stringify({
-            uname: "testUser",
-            savedWorkouts: {},
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-        .then(response => response.json())
-        .then(data => console.log(data)); 
+
+    console.log(targetDates)
+    send_Data("delete", "DELETE", {targetDates: targetDates})
 }
 
-fetch('https://lennsflask.duckdns.org/api/etrack_users/')
+fetch('http://localhost:8086/api/etrack_users/')
   .then((response) => response.json())
-  .then((data) => this.applydata(data[0]));
+  .then((data) => this.applydata(data));
 
 function applydata(data) {
-    for (const [k, v] of Object.entries(data["savedWorkouts"])) {
-        savedEvents[k] = v
+    console.log(data);
+    for (const row of data) {
+        console.log(row);
+        console.log(row["date"]);
+        savedEvents[row["date"]] = row["savedWorkouts"]
     }
+    console.log(savedEvents);
     applySavedData();
 }
 
@@ -145,10 +153,10 @@ function applySavedData() {
     notemptyvalues= {};
     for (const [k, v] of Object.entries(savedEvents)) {
         document.getElementById(k.split(" ")[0]).style.color="";
-        if (!Array.isArray(v) || v.length) {
+            if (!Array.isArray(v) || v.length) {
             notemptyvalues[k] = v;
             document.getElementById(k.split(" ")[0]).style.color="cyan";
-        }
+            }
     }
     scheduledEvents.innerHTML = '';
     const workoutTable = document.createElement("table");
@@ -182,16 +190,20 @@ function openEditPopup(day) {
 
 function editDate() {
     input = document.getElementById("eventDayInput").value
+
     console.log("input is " + input);
     console.log("clicked date is " + clickedDate);
-    if (input + ' ' + document.getElementById("calendarHeader").innerText in savedEvents) {
-        savedEvents[input + ' ' + document.getElementById("calendarHeader").innerText] = savedEvents[clickedDate + ' ' + document.getElementById("calendarHeader").innerText]
-        savedEvents[clickedDate + ' ' + document.getElementById("calendarHeader").innerText] = []
+    if (input + monthAndYear in savedEvents) {
+        savedEvents[input + monthAndYear] = savedEvents[clickedDate + monthAndYear]
+        send_Data("create", "POST", {date: input + monthAndYear, savedWorkouts: savedEvents[input + monthAndYear]})
+        savedEvents[clickedDate + monthAndYear] = []
+        send_Data("delete", "DELETE", {targetDates: [clickedDate + monthAndYear]})
         applySavedData();
-        PATCH_notemtpyvalues();
+        // send_Data();
     }
     console.log(savedEvents);
     document.getElementById("eventDayInput").value = '';
     closePopup();
 }
 renderCalendar();
+let monthAndYear = ' ' + document.getElementById("calendarHeader").innerText
