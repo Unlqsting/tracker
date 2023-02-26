944dd2da045ee11a782a357448d03f9503c4a89f
SAnd353 <91165049+SAnd353@users.noreply.github.com>
2023-02-25 16:32:20 -0800

    comments + slight adjustment to date editing

--- calendar.js
+++ calendar.js
@@ -2,6 +2,7 @@ var savedEvents = {
     
 }
 
+// Defining a function for non-get fetch requests for conciseness
 function send_Data(endpoint, method, body) {
     fetch("https://lennsflask.duckdns.org/api/etrack_users/" + endpoint, {
         method: method,
@@ -14,6 +15,7 @@ function send_Data(endpoint, method, body) {
         .then(data => console.log(data)); 
 }
 
+// Defining a function for when workouts are added
 function updateCal() {
     console.log(savedEvents);
     closePopup();
@@ -37,11 +39,13 @@ function updateCal() {
     // send_Data();
 }
 
+// Defining some document elements as variables for future use
 const newEventPopup = document.getElementById('newEventPopup');
 const editEventPopup = document.getElementById('editEventPopup');
 const backDrop = document.getElementById('PopupBackDrop');
 const eventTitleInput = document.getElementById('eventTitleInput');
 
+// displays the popup for new events
 function openPopup(date) {
     clicked = date;
     // const eventForDay = events.find(e => e.date === clicked);
@@ -56,6 +60,7 @@ function openPopup(date) {
     backDrop.style.display = 'block';
 }
 
+//universal popup closing function
 function closePopup() {
     editEventPopup.style.display = 'none';
     newEventPopup.style.display= 'none';
@@ -69,8 +74,9 @@ let clicked = null;
 
 const calendar = document.getElementById('calendar');
 
+// Creating the actual calendar
 function renderCalendar() {
-
+    // Defining necessary variables for calendar generation using javascript's date functionality
     const date = new Date();
 
     const day = date.getDate();
@@ -78,7 +84,7 @@ function renderCalendar() {
     const year = date.getFullYear();
     const daysInMonth = new Date(year, month + 1, 0).getDate();
     const firstDayOfMonth = new Date(year, month, 1);
-    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; // necessary for padding day generation
 
     const dateString = firstDayOfMonth.toLocaleDateString('en-US', {
         weekday: 'long',
@@ -91,13 +97,13 @@ function renderCalendar() {
 
 
     document.getElementById('calendarHeader').innerText = 
-    `${date.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+    `${date.toLocaleDateString('en-us', { month: 'long' })} ${year}`; // Setting month
 
 
     calendar.innerHTML = '';
 
-    const prevDays = weekdays.indexOf(dateString.split(', ')[0]);
-    for(let i = 1; i <= prevDays + daysInMonth; i++) {
+    const prevDays = weekdays.indexOf(dateString.split(', ')[0]); // Determining how many days in the month's first week are from the previous month
+    for(let i = 1; i <= prevDays + daysInMonth; i++) { // prevDays + daysInMonth gives the number of daysquares to generate
         const daySquare = document.createElement('div');
         daySquare.classList.add('day');
 
@@ -105,7 +111,7 @@ function renderCalendar() {
             daySquare.innerText = i - prevDays;
             daySquare.setAttribute("id", i - prevDays)
             daySquare.addEventListener('click', () => openPopup(daySquare.innerHTML));
-            savedEvents[daySquare.innerHTML + ' ' + document.getElementById("calendarHeader").innerText] = Array();
+            savedEvents[daySquare.innerHTML + ' ' + document.getElementById("calendarHeader").innerText] = Array(); // defining events array for each day
         } 
         else {
             daySquare.classList.add('padding');
@@ -119,7 +125,7 @@ function clearWorkouts() {
     console.log(savedEvents);
     targetDates = [];
     console.log(notemptyvalues);
-    for (const k in notemptyvalues) {
+    for (const k in notemptyvalues) { // finding which dates exist in backend
         console.log("Pushing " + k)
         targetDates.push(k);
     }
@@ -130,14 +136,14 @@ function clearWorkouts() {
     applySavedData();
 
     console.log(targetDates)
-    send_Data("delete", "DELETE", {targetDates: targetDates})
+    send_Data("delete", "DELETE", {targetDates: targetDates}) // deleting entries in BE table
 }
 
-fetch('https://lennsflask.duckdns.org/api/etrack_users/')
+fetch('https://lennsflask.duckdns.org/api/etrack_users/') // simple GET request
   .then((response) => response.json())
   .then((data) => this.applydata(data));
 
-function applydata(data) {
+function applydata(data) { // Loading data from BE into savedEvents
     console.log(data);
     for (const row of data) {
         console.log(row);
@@ -153,17 +159,16 @@ function applySavedData() {
     notemptyvalues= {};
     for (const [k, v] of Object.entries(savedEvents)) {
         document.getElementById(k.split(" ")[0]).style.color="";
-            if (!Array.isArray(v) || v.length) {
+            if (!Array.isArray(v) || v.length) { // getting days with events saved
             notemptyvalues[k] = v;
-            document.getElementById(k.split(" ")[0]).style.color="cyan";
+            document.getElementById(k.split(" ")[0]).style.color="cyan"; // day highlighting
             }
     }
     scheduledEvents.innerHTML = '';
     const workoutTable = document.createElement("table");
     const workoutTableBody = document.createElement("tbody");
 
-    rowIndex = 1
-    for (const [k, v] of Object.entries(notemptyvalues)) {
+    for (const [k, v] of Object.entries(notemptyvalues)) { //building table
         const row = document.createElement("tr");
         const dateCell = document.createElement("td");
         dateCell.innerText = k;
@@ -182,24 +187,25 @@ function applySavedData() {
     scheduledEvents.appendChild(workoutTable);
 }
 
-function openEditPopup(day) {
+function openEditPopup(day) { //displays the edit popup
     clickedDate = day;
     editEventPopup.style.display = 'block';
     backDrop.style.display = 'block';
 }
 
 function editDate() {
-    input = document.getElementById("eventDayInput").value
+    input = document.getElementById("eventDayInput").value //retrieving input
 
     console.log("input is " + input);
     console.log("clicked date is " + clickedDate);
-    if (input + monthAndYear in savedEvents) {
-        savedEvents[input + monthAndYear] = savedEvents[clickedDate + monthAndYear]
-        send_Data("create", "POST", {date: input + monthAndYear, savedWorkouts: savedEvents[input + monthAndYear]})
-        savedEvents[clickedDate + monthAndYear] = []
-        send_Data("delete", "DELETE", {targetDates: [clickedDate + monthAndYear]})
-        applySavedData();
-        // send_Data();
+    if (input + monthAndYear in savedEvents) { //checking input
+        if (savedEvents[input + monthAndYear].length == 0) { //checking if inputted date has workouts already
+            savedEvents[input + monthAndYear] = savedEvents[clickedDate + monthAndYear] //switching
+            send_Data("create", "POST", {date: input + monthAndYear, savedWorkouts: savedEvents[input + monthAndYear]})
+            savedEvents[clickedDate + monthAndYear] = []
+            send_Data("delete", "DELETE", {targetDates: [clickedDate + monthAndYear]})
+            applySavedData();
+        }
     }
     console.log(savedEvents);
     document.getElementById("eventDayInput").value = '';
